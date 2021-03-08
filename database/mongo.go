848d5d02da125637523164e392984f6fc4005c2a
vn0jsak <abisaid.farias@wallmart.com>
2021-03-07 23:46:43 -0300

    ajsutes en el payload del create user

--- database/mongo.go
+++ database/mongo.go
@@ -2,24 +2,21 @@ package database
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
 //Conn on mongo
 func Conn() *mongo.Database {
-	host := "localhost"
-	port := 27017
 
-	clientOpts := options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%d", host, port))
+	clientOpts := options.Client().ApplyURI("mongodb://lbtechbd-dev:cZ6JkSeY6qtSNO1KRhFqR3RksRtMbQjPXYc0jhDM9RRMXH1i1RxHpOmmQxi3YeRRYQmWL42TK6rr5xIWSKwoyg%3D%3D@lbtechbd-dev.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@lbtechbd-dev@")
 	client, err := mongo.Connect(context.TODO(), clientOpts)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	
 
 	err = client.Ping(context.TODO(), nil)
 	if err != nil {
--- viewmodels/request/user.request.go
+++ viewmodels/request/user.request.go
@@ -2,10 +2,10 @@ package request
 
 // UserRequest response to client
 type UserRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"passwordHash"`
-	Name     string `json:"name"`
-	LastName string `json:"lastName"`
-	Phone    string `json:"phone"`
-	Dni      string `json:"dni"`
+	Email    string `json:"email" bson:"email" binding:"required"`
+	Password string `json:"password" bson:"password" binding:"required"`
+	Name     string `json:"name" bson:"name" binding:"required"`
+	LastName string `json:"lastName" bson:"lastName" binding:"required"`
+	Phone    string `json:"phone" bson:"phone"`
+	Dni      string `json:"dni" bson:"dni" binding:"required"`
 }
