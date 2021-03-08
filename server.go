f19d908df6869ab7bbd253fca59d76e499977bab
Manuel Da Silva <manueldasilva98@gmail.com>
2021-03-08 11:24:45 -0400

    removed comment

--- server.go
+++ server.go
@@ -10,7 +10,7 @@ import (
 	"github.com/abisaidfarias/lbtechapi/utils"
 
 	// "github.com/abisaidfarias/lbtechapi/middlewares"
-	"github.com/abisaidfarias/lbtechapi/models"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -30,6 +30,7 @@ func main() {
 	server := gin.Default()
 
 	// server.Use(gindump.Dump())
+	// server.Use(middlewares.AuthMiddleware())
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("passwordFormat", utils.ValidPassword)
@@ -41,30 +42,9 @@ func main() {
 		auth.POST("/create", userController.Create())
 	}
 
-	// server.Use(middlewares.AuthMiddleware())
-	// server.Use(testAuthMiddleware())
-
 	server.GET("/health", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"status": "server is up"})
 	})
 
 	log.Fatal(server.Run(":8080"))
-}
-
-func testAuthMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-		requestUser, exists := c.Get("user")
-
-		if !exists {
-			log.Printf("Unable to extract user from request context for unknown reason: %v\n", c)
-		} else {
-			user := requestUser.(*models.User)
-			log.Println(user.ID)
-			log.Println(user.Email)
-		}
-		// before request
-		c.Next()
-
-	}
 }
