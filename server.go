39270c27da3d25e89c0150da9fdc4038e6fa0d3d
Manuel Da Silva <manueldasilva98@gmail.com>
2021-03-09 17:01:34 -0400

    open cors

--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/abisaidfarias/lbtechapi/repositories"
 	"github.com/abisaidfarias/lbtechapi/services"
 	"github.com/abisaidfarias/lbtechapi/utils"
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
@@ -28,6 +29,8 @@ func main() {
 
 	server := gin.Default()
 	// server.Use(gindump.Dump())
+
+	server.Use(cors.Default())
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("passwordFormat", utils.ValidPassword)
