ef53458dd79acd3f1a4d6eca4719421700713103
Manuel Da Silva <manueldasilva98@gmail.com>
2021-05-06 16:50:33 -0400

    severity and frequency, fix test model

--- viewmodels/request/test_result_resume.go
+++ viewmodels/request/test_result_resume.go
@@ -2,9 +2,11 @@ package request
 
 type TestResultResume struct {
 	Code             string      `bson:"code" json:"code" binding:"required"`
-	Result           int         `bson:"result" json:"result" binding:"required"`
+	Result           int         `bson:"result" json:"result"`
 	IssueDescription string      `bson:"issue_description" json:"issue_description"`
 	Images           []string    `bson:"images" json:"images"`
 	Hyperlinks       []Hyperlink `bson:"hyperlinks" json:"hyperlinks"`
 	Value            string      `bson:"value" json:"value"`
+	IssueFrequency   int         `bson:"issue_frequency" json:"issue_frequency"`
+	IssueSeverity    int         `bson:"issue_severity" json:"issue_severity"`
 }
